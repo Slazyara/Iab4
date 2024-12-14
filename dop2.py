96f2ca136a8c8bb7c5624079149f84acbd047806
unknown <sashailichev6002@gmail.com>
2024-12-14 17:58:46 +0300

    d

--- dop2.py
+++ dop2.py
@@ -6,16 +6,17 @@ with open('schedule.json', encoding='utf-8') as f:
 outfile = open("schedule2.yaml", 'w', encoding='utf-8')
 
 for x in json_array:
+    x=x.replace(" ",'',2)
     a = re.split(r':\s*', x, maxsplit=1)
+
     if len(a) > 1:
         a[0] = re.sub(r'"', '', a[0])
 
         a[1] = re.sub(r'"', '', a[1])
-        a[1] = re.sub(r'[,\{\[]$', '', a[1])
-
-        outfile.write(a[0] + ': ' + a[1] + "\n")
+        a[1] = re.sub(r'[,\{[\][]', '', a[1])
+        outfile.write(a[0] + ':' + a[1] + "\n")
 
 outfile.close()
-print("Конвертация завершена. Результат сохранен в 'schedule2    .yaml'.")
+print("Конвертация завершена. Результат сохранен в 'schedule2.yaml'.")
 
 
