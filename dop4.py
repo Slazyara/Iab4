c0b3dd08c6c37e2e4086814269a464510cb0f02a
unknown <sashailichev6002@gmail.com>
2024-12-14 17:35:04 +0300

    my changes

--- dop4.py
+++ dop4.py
@@ -6,7 +6,7 @@ for i in range(100):
     import task1
 end_time = time.perf_counter()
 am=end_time- start_time
-print(f"Основа - {end_time - start_time}")
+#print(f"Основа - {end_time - start_time}")
 
 start_time = time.perf_counter()
 for i in range(100):
@@ -14,7 +14,7 @@ for i in range(100):
 end_time = time.perf_counter()
 a1=end_time- start_time
 
-print(f"Доп 1  - {end_time - start_time}")
+#print(f"Доп 1  - {end_time - start_time}")
 
 start_time = time.perf_counter()
 for i in range(100):
@@ -22,7 +22,7 @@ for i in range(100):
 end_time = time.perf_counter()
 a2=end_time- start_time
 
-print(f"Доп 2  - {end_time - start_time}")
+#print(f"Доп 2  - {end_time - start_time}")
 
 start_time = time.perf_counter()
 for i in range(100):
@@ -30,6 +30,11 @@ for i in range(100):
 end_time = time.perf_counter()
 a5=end_time- start_time
 
-print(f"Доп 5  - {end_time - start_time}")
+#print(f"Доп 5  - {end_time - start_time}")
 
+
+print(f"Основное задание- {am}")
+print(f"Доп 1- {a1}")
+print(f"Доп 2- {a2}")
+print(f"Доп 5- {a5}")
 #print(am,"  ",a1,"  ",a2,"   ",a5)
--- dop5.py
+++ dop5.py
@@ -4,10 +4,10 @@ with open('schedule.json', 'r', encoding='utf-8') as json_file:
     data = json.load(json_file)
 
 with open('timetable.csv', 'w', encoding='utf-8') as f:
-    f.write('День,Пара,Предмет,Время,Недели,Преподаватель,Здание,Кабинет,Формат\n')
+    f.write('День;Пара;Предмет;Время;Недели;Преподаватель;Здание;Кабинет;Формат\n')
     for day_name, day_data in data['timetable'].items():
         for order, lesson in day_data['lessons'].items():
             f.write(
-                f"{day_name},{order},{lesson['subject']},{lesson['time']},{lesson['weeks']},{lesson['teacher']},{lesson['building']},{lesson['room']},{lesson['format']}\n")
+                f"{day_name};{order};{lesson['subject']};{lesson['time']};{lesson['weeks']};{lesson['teacher']};{lesson['building']};{lesson['room']};{lesson['format']}\n")
 
 print("Конвертация завершена. Результат сохранен в 'timetable.csv'.")
