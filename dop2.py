7af577c978c876069838b31673a90476c2e14885
Slazyara <sashailichev6002@gmail.com>
2024-12-14 18:49:00 +0300

    Update dop2.py

--- dop2.py
+++ dop2.py
@@ -7,7 +7,7 @@ outfile = open("schedule2.yaml", 'w', encoding='utf-8')
 
 for x in json_array:
     x=x.replace(" ",'',2)
-    a = re.split(r':\s*', x, maxsplit=1)
+    a = re.split(r':', x, maxsplit=1)
 
     if len(a) > 1:
         a[0] = re.sub(r'"', '', a[0])
